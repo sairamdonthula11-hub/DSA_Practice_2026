14c91bab31a9b3d840e3b4fce63b0b7584d74590
donthulasairam <sairamdonthula11@gmail.com>
2026-03-31 15:32:39 +0530

    Added Two Sum Solution with explanation

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,68 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello, Welcome!");
+
+        /*
+        * Two Sum
+         */
+
+        TwoSum twoSum = new TwoSum();
+
+        int[] twoSumNums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] twoSumResult1 = twoSum.twoSumOptimized(twoSumNums1, target1);
+        if (twoSumResult1[0] != -1 && twoSumResult1[1] != -1) {
+
+            System.out.println("\"" + Arrays.toString(twoSumNums1) + "\"" + " : Numbers, " + "\"" + twoSumNums1[twoSumResult1[0]] + "\"" + " and " + "\"" + twoSumNums1[twoSumResult1[1]] + "\"" + " adds upto target, " + "\"" + target1 + "\"");
+
+        } else {
+
+            System.out.println("\"" + Arrays.toString(twoSumNums1) + "\"" + " : No Two Number Found that that adds upto target, " + target1);
+
+        }
+
+        int[] twoSumNums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] twoSumResult2 = twoSum.twoSumOptimized(twoSumNums2, target2);
+        if (twoSumResult2[0] != -1 && twoSumResult2[1] != -1) {
+
+            System.out.println("\"" + Arrays.toString(twoSumNums2) + "\"" + " : Numbers, " + "\"" + twoSumNums2[twoSumResult2[0]] + "\"" + " and " + "\"" + twoSumNums2[twoSumResult2[1]] + "\"" + " adds upto target, " + "\"" + target2 + "\"");
+
+        } else {
+
+            System.out.println("\"" + Arrays.toString(twoSumNums2) + "\"" + " : No Two Number Found that that adds upto target, " + target2);
+
+        }
+
+        int[] twoSumNums3 = {3, 3};
+        int target3 = 6;
+        int[] twoSumResult3 = twoSum.twoSumOptimized(twoSumNums3, target3);
+        if (twoSumResult3[0] != -1 && twoSumResult3[1] != -1) {
+
+            System.out.println("\"" + Arrays.toString(twoSumNums3) + "\"" + " : Numbers, " + "\"" + twoSumNums3[twoSumResult3[0]] + "\"" + " and " + "\"" + twoSumNums3[twoSumResult3[1]] + "\"" + " adds upto target, " + "\"" + target3 + "\"");
+
+        } else {
+
+            System.out.println("\"" + Arrays.toString(twoSumNums3) + "\"" + " : No Two Number Found that that adds upto target, " + target3);
+
+        }
+
+        int[] twoSumNums4 = {2, 7, 11, 15, 17, 19, 20};
+        int target4 = 1;
+        int[] twoSumResult4 = twoSum.twoSumOptimized(twoSumNums4, target4);
+        if (twoSumResult4[0] != -1 && twoSumResult4[1] != -1) {
+
+            System.out.println("\"" + Arrays.toString(twoSumNums4) + "\"" + " : Numbers, " + "\"" + twoSumNums4[twoSumResult4[0]] + "\"" + " and " + "\"" + twoSumNums4[twoSumResult4[1]] + "\"" + " adds upto target, " + "\"" + target4 + "\"");
+
+        } else {
+
+            System.out.println("\"" + Arrays.toString(twoSumNums4) + "\"" + " : No Two Number Found that that adds upto target, " + target4);
+
+        }
 
     }
 
---
+++ src/TwoSum.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+
+public class TwoSum {
+
+    /**
+     * Brute Force:
+     * for i : nums.size()
+     *  for j : nums.size()
+     *      if (nums[i] + nums[j] == target)
+     *          return [i, j];
+     * Time Complexity: O(n * n)
+     * Space Complexity: O(1)
+     *
+     * Optimized Approach:
+     * Pattern: 3. Need fast lookup / Frequency Count? -> HashMap / HashSet
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     */
+
+    public int[] twoSumOptimized(int[] nums, int target) {
+
+        HashMap<Integer, Integer> missingPieceMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // Missing Piece
+            int complement = target - nums[i];
+
+            // Checking if anyone already has this missing piece that adds up to target
+            if (missingPieceMap.containsKey(complement)) {
+
+                return new int[] {missingPieceMap.get(complement), i};
+
+            }
+
+            // No matches
+            missingPieceMap.put(nums[i], i);
+
+        }
+
+        // Didn't find any
+        return new int[] {-1, -1};
+
+    }
+
+}
