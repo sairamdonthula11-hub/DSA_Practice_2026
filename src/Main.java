2a8eccc7b436ec3e0fa767f50d5f8fc6228a1002
donthulasairam <sairamdonthula11@gmail.com>
2026-03-31 11:54:11 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-public class Main {
-
-    public static void main(String[] args) {
-
-        System.out.println("Hello, Thank you!");
-
-    }
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Hello and Welcome!"));
 
 }
